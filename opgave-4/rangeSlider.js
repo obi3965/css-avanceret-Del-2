f95b185a60115daa2fa2950a16d2cbbe7214d678
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-04-24 12:50:12 +0200

    commit

--- opgave-4/rangeSlider.js
+++ opgave-4/rangeSlider.js
@@ -1,11 +1,40 @@
 
-let rangeSlider = document.querySelector("#rangeSlider");
+// let rangeSlider = document.querySelector("#rangeSlider");
 
-let rangeSliderOutput = document.querySelector("#rangeValue");
+// let rangeSliderOutput = document.querySelector("#rangeValue");
 
-rangeSliderOutput.innerHTML= rangeSlider.value;
+// rangeSliderOutput.innerHTML= rangeSlider.value;
 
-rangeSlider.oninput = function(){
-    rangeSliderOutput.innerHTML = this.value;
+// rangeSlider.oninput = function(){
+//     rangeSliderOutput.innerHTML = this.value;
+// }
+
+
+
+
+
+
+var red_slider = document.getElementById("red_slider");
+var green_slider = document.getElementById("green_slider");
+var blue_slider = document.getElementById("blue_slider");
+
+var red_output = document.getElementById("red_value");
+var green_output = document.getElementById("green_value");
+var blue_output = document.getElementById("blue_value");
+
+
+red_slider.oninput = function() {
+  red_output.innerHTML = this.value;
+  document.documentElement.style.setProperty('--red-color', this.value);
+
+}
+green_slider.oninput = function() {
+  green_output.innerHTML = this.value;
+  document.documentElement.style.setProperty('--green-color', this.value);
+
+}
+blue_slider.oninput = function() {
+  blue_output.innerHTML = this.value;
+  document.documentElement.style.setProperty('--blue-color', this.value);
 }
 
